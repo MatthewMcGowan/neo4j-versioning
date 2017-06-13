9b356cb49a5be331abad9f5006684e18ebcc53ee
Matthew McGowan <mattmcgowan1989@hotmail.com>
2017-06-13 14:39:05 +0100

    Created initial project structure.

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "uk.co.matthewmcgowan.neo4j-versioning"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+
+
+libraryDependencies += "com.typesafe" % "config" % "1.3.1"
---
+++ src/main/scala/Deploy.scala
@@ -0,0 +1,10 @@
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Created by Matthew.McGowan on 13/06/2017.
+  */
+object Deploy extends App {
+  val conf = ConfigFactory.load()
+
+  
+}
